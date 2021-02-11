266c183845494b1f3f82809bcee538983af6286d
Patrick Roos <pa.roos@icloud.com>
2021-02-11 11:37:40 +0100

    insertOne instead of insert

--- 10-Access-mongodb/demo.js
+++ 10-Access-mongodb/demo.js
@@ -8,7 +8,7 @@ const url = "mongodb+srv://<<db-user>>:<<pw>>@<<server-url>>/<<your-db>>";
         client = await MongoClient.connect(url);
         console.log('Connected correctly to server');
         const db = client.db('db');
-        let r = await db.collection('students').insert({text: 'hello web programming lab 3'});
+        let r = await db.collection('students').insertOne({text: 'hello web programming lab 3'});
         console.log(r);
     } catch(err) {
         console.log(err);
