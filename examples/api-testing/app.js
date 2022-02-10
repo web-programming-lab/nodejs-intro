9eee074371ccafa825bd84a32bc6dd81cdbe0534
Patrick Roos <pa.roos@icloud.com>
2022-02-10 10:35:14 +0100

    Fix app for api testing example

--- examples/api-testing/app.js
+++ examples/api-testing/app.js
@@ -15,11 +15,27 @@ server.get('/technologies', async (req, res) => {
   res.send(result);
 });
 
+server.get('/technologies/:id', async(req, res) => {
+    const client = await mongoClient.connect(connectionString);
+    const db = client.db('techradar');
+    const collection = db.collection('technologies');
+    const result = await collection.findOne({_id: objectId(req.params.id)});
+    
+    if (result) {
+        res.send(result);
+    } else {
+        res.status(404);
+    }
+
+    res.end();
+});
+
 server.post("/technologies", async (req, res) => {
   const client = await mongoClient.connect(connectionString);
   const db = client.db('techradar');
   const collection = db.collection('technologies');
-  const result = await collection.save(req.body);
+  const result = await collection.insertOne(req.body);
+
   res.status(201);
   res.end();
 });
