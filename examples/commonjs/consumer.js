448cc2636efea4de33a93090818b370752ccf0be
Patrick Roos <pa.roos@icloud.com>
2023-02-08 13:47:50 +0100

    arguments added to show the commonjs function wrapper

--- examples/commonjs/consumer.js
+++ examples/commonjs/consumer.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line no-undef
+console.log(arguments); // CommonJS modules are wrapped in a function
+
 const output = require('./output.cjs');
 output.log('hello web programming lab!');
 output.log(`${output.myVar}`);
